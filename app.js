04c99aaaab44fbec5f5ea20ab5fcaa5ab48b6f05
Petr Gilevich <unvir@yandex.ru>
2017-12-22 20:01:24 +0300

    Design of pick datetime page

--- app.js
+++ app.js
@@ -2,62 +2,47 @@ var app = {
     API_VERSION: '5.69',
     API_SETTINGS_SCOPE_PHOTOS: 4,
     VIEWER_DEVICE_MOBILE: 'mobile',
-    MAX_HOLD_DAYS: 48,
+    MAX_HOLD_DAYS: 10,
     MINUTES_PER_HOUR: 60,
     HOURS_PER_DAY: 24,
-    HOUR_SEPARATOR: 0.01, // 1 - EVERY HOUR, 2 - EVERY 30 MIN, 4 - EVERY 15 MIN
+    HOUR_SEPARATOR: 2, // 1 - EVERY HOUR, 2 - EVERY 30 MIN, 4 - EVERY 15 MIN
 
     pages: {
         install: document.getElementById('page-install'),
         pickDatetime: document.getElementById('page-pick-datetime')
     },
     elements: {
-        timeTable: document.getElementById('table-datetime-picker')
+        dateSelector: document.getElementById('select-date'),
+        timeSelector: document.getElementById('select-time')
     },
     groupId: 0,
     appId: 0,
 
-    fillPickerTable: function (table) {
-        table.innerHTML = /*app.generatePickerTableHead() +*/ app.generatePickerTableBody();
-    },
-
-    generatePickerTableHead: function () {
+    fillDateSelector: function (selectElement) {
+        selectElement.innerHTML = '';
         var currentDay = new Date().getDate();
-        var tableHtml = '<thead><tr>';
 
         for (var i = 0; i < app.MAX_HOLD_DAYS; i++) {
             var nextDate = new Date(new Date().setDate( currentDay + i ));
-            tableHtml += '<th>' + nextDate.toLocaleDateString() + '</th>';
+            selectElement.innerHTML += '<option>' + nextDate.toLocaleDateString() + '</option>';
         }
-        tableHtml += '</tr></thead>';
-        return tableHtml;
     },
 
-    generatePickerTableBody: function () {
-        var tableHtml = '<tbody>';
+    generateTimePickerList: function () {
+        var tableHtml = '';
 
         for (var i = 0; i < app.HOURS_PER_DAY * app.MINUTES_PER_HOUR; i += app.MINUTES_PER_HOUR / app.HOUR_SEPARATOR) {
-            var rowHtml = '<tr>';
-            var columnsHtml = '';
-
             var date = new Date(new Date(0).setHours(0));
             var time = new Date(date.setMinutes(i));
 
-            for (var j = 0; j < app.MAX_HOLD_DAYS;  j++) {
-                columnsHtml += '<td>' + time.toLocaleTimeString().split(/:00$/)[0] + '</td>';
-            }
-            rowHtml += columnsHtml + '</tr>';
-            tableHtml += rowHtml;
+            tableHtml += '<li>' + time.toLocaleTimeString().split(/:00$/)[0] + '</li>';
         }
-        tableHtml += '</tbody>';
         return tableHtml;
     },
 
     init: function () {
-        app.fillPickerTable(app.elements.timeTable);
-        // document.getElementsByClassName('responsive')[0].addEventListener('scroll', function (event) {
-        //     document.querySelector('#table-datetime-picker thead').style.top = event.target.scrollTop + 'px'; //Это временно
-        // });
+        app.fillDateSelector(app.elements.dateSelector);
+        app.elements.timeSelector.innerHTML = app.generateTimePickerList();
     }
 };
 
