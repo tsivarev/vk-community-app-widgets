c06a715281e85975ce9ced8d9d894a56678c74ec
Petr Gilevich <unvir@yandex.ru>
2017-12-26 11:33:32 +0300

    Complete to review

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 var app = {
     API_VERSION: '5.69',
-    API_SETTINGS_SCOPE_PHOTOS: 4,
-    VIEWER_DEVICE_MOBILE: 'mobile',
     MAX_HOLD_DAYS: 10,
     MINUTES_PER_HOUR: 60,
     HOURS_PER_DAY: 24,
     HOUR_SEPARATOR: 2, // 1 - EVERY HOUR, 2 - EVERY 30 MIN, 4 - EVERY 15 MIN
 
     pages: {
-        // install: document.getElementById('page-install'),
         pickDatetime: document.getElementById('page-pick-datetime'),
         pickRoom: document.getElementById('page-pick-room')
     },
@@ -16,8 +13,6 @@ var app = {
         dateSelector: document.getElementById('select-date'),
         timeSelector: document.getElementById('list-select-time')
     },
-    groupId: 0,
-    appId: 0,
 
     show: function (page) {
         app.hideAll();
